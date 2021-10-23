4c926549d7a30fbe98fe9ab1d6f1512667ac22cd
heyyayesh <biswalayesh3@gmail.com>
2021-10-23 11:56:20 +0530

    Add basic html interface to the page

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a web app that lets users play the classic game of Rock-Paper-Scissors
+with the computer. This project was done as an assignment while following
+through The Odin Project.
--- script.js
+++ script.js
@@ -1,69 +1,98 @@
+let playerSelection;
+let computerSelection;
+
+const buttons = document.querySelectorAll('.choice');
+
+const playerChoice = document.getElementById('playerChoice');
+const computerChoice = document.getElementById('computerChoice');
+
+const roundResult = document.getElementById('roundResult');
+const score = document.getElementById('score');
+
+const winner = document.getElementById('winner');
+const reset = document.getElementById('reset');
+
+let pScore = 0, cScore = 0;
+
 function coumputerPlay(){
-    const choice = ['rock', 'paper', 'scissors'];
+    const choice = ['Rock', 'Paper', 'Scissors'];
     let index = Math.floor(Math.random()*3);
     return choice[index];
 }
 
-const playerSelection = '';
-const computerSelection = '';
+buttons.forEach(button => button.addEventListener('click', function(e) {
 
-let pScore = 0, cScore = 0;
+    playerSelection = e.target.textContent;
+    computerSelection = coumputerPlay();
+
+    playerChoice.textContent = playerSelection;
+    computerChoice.textContent = computerSelection;
+
+    roundResult.textContent = playRound(playerSelection, computerSelection);
+    score.textContent = `You : ${pScore} VS ${cScore} : Computer`;
+
+    if(declareWinner()[0]){
+        if(declareWinner()[1] == 'p') winner.textContent = 'You win! Congratulations!';
+        else if(declareWinner()[1] == 'c') winner.textContent = 'The mighty computer wins. Better luch next time!';
+    }
+}));  
+
+reset.addEventListener('click', () => {
+    pScore = 0, cScore = 0;
+    playerChoice.textContent = 'None';
+    computerChoice.textContent = 'None';
+    roundResult.textContent = 'You haven\'t played a round yet';
+    roundResult.style.backgroundColor = 'white';
+    score.textContent = `You : 0 VS 0 : Computer`;
+    winner.textContent = 'Winner : None';
+});
+
+function declareWinner(){
+    let w;
+
+    if(pScore == 5) w = 'p';
+    else if(cScore == 5) w = 'c';
+
+    return [pScore == 5 || cScore == 5, w];
+}
 
 function playRound(p, c){
-    
-    p = prompt('Enter your choice: ');
-    c = coumputerPlay();
 
     if(p.toUpperCase() === c.toUpperCase()){
+        roundResult.style.backgroundColor = 'yellow';
         return 'Tie!';
     }
     else if(p.toUpperCase() === 'ROCK' && c.toUpperCase() === 'PAPER'){
         cScore++;
-        return 'You lose! Paper beats Rock!';
+        roundResult.style.backgroundColor = 'red';
+        return 'Paper beats Rock!';
     }
     else if(p.toUpperCase() === 'ROCK' && c.toUpperCase() === 'SCISSORS'){
         pScore++;
-        return 'You win! Rock beats Scissors!';
+        roundResult.style.backgroundColor = 'lime';
+        return 'Rock beats Scissors!';
     }
     else if(p.toUpperCase() === 'PAPER' && c.toUpperCase() === 'ROCK'){
         pScore++;
-        return 'You win! Paper beats Rock!';
+        roundResult.style.backgroundColor = 'lime';
+        return 'Paper beats Rock!';
     }
     else if(p.toUpperCase() === 'PAPER' && c.toUpperCase() === 'SCISSORS'){
         cScore++;
-        return 'You lose! Scissors beat Paper!';
+        roundResult.style.backgroundColor = 'red';
+        return 'Scissors beat Paper!';
     }
     else if(p.toUpperCase() === 'SCISSORS' && c.toUpperCase() === 'ROCK'){
         cScore++;
-        return 'You lose! Rock beats Scissors!';
+        roundResult.style.backgroundColor = 'red';
+        return 'Rock beats Scissors!';
     }
     else if(p.toUpperCase() === 'SCISSORS' && c.toUpperCase() === 'PAPER'){
         pScore++;
-        return 'You win! Scissors beat Paper!';
+        roundResult.style.backgroundColor = 'lime';
+        return 'Scissors beat Paper!';
     }
     else{
         return 'Invalid choice!';
     }
 }
-
-
-
-function game(){
-    for(let i=0; i<5; i++){
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    
-    if(pScore === cScore){
-        console.log(`Your score = ${pScore}\nOpponent's score = ${cScore}\nIt's a Tie!`);
-    }
-
-    if(pScore > cScore){
-        console.log(`Your score = ${pScore}\nOpponent's score = ${cScore}\nYou Win!`);
-    }
-
-    if(pScore < cScore){
-        console.log(`Your score = ${pScore}\nOpponent's score = ${cScore}\nYou Lose!`);
-    }
-}
-
-game();
